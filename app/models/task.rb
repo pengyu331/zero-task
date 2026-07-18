a38cd36b0299a50f8ec70c407e64268e845d55e3
pengyu <chpy15@gmail.com>
2026-07-19 01:21:12 +0800

    fix erb_lint

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,10 +8,12 @@ class Task < ApplicationRecord
   validates :description, presence: true, length: {maximum: 140}
 
   scope :recent, -> { order(created_at: :desc) }
-  scope :search_by_keyword, ->(keyword) {
-    where("description LIKE ?", "%#{sanitize_sql_like(keyword)}%") if keyword.present?
-  }
-  scope :filter_by_state, ->(state_param) {
-    where(state: state_param) if state_param.present? && state_param != 'all'
-  }
+  scope :search_by_keyword,
+    lambda { |keyword|
+      where("description LIKE ?", "%#{sanitize_sql_like(keyword)}%") if keyword.present?
+    }
+  scope :filter_by_state,
+    lambda { |state_param|
+      where(state: state_param) if state_param.present? && state_param != "all"
+    }
 end
